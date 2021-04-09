32b8930097ae83a59b1860538490ffcb6916e2a9
Roman <romanvasylyna888@gmail.com>
2021-04-09 17:40:53 +0300

    Select Done

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 
 // нужные поля index, name, region, population, area, flag
-// 1) вывести табличку с полями
-// 2) 
+// 2) фильтрация через поисковик. вводим символы - в таблице отображены 
+// страны с частичным совпадением по name, capital, region
+
+// Очищаем tbody
+const clearElem = selector => {        
+$(`${selector}`).html("");    
+$(`${selector}`).val(""); 
+}
 
 // Задаем события
-const setListeners = () => {
+const setListeners = (object) => {
+
+// Событие для селекта    
+// Меняется опция - фильтруются страны согласно региону
+$('#select').change(e => {
+clearElem('#table tbody');
+let newRegions = object.filter(elem => elem.region == e.currentTarget.value);
+console.log(newRegions);
+addToDom(newRegions);
+});
+
+// Событие для инпута
+$('#search').keyup(e => {    
+// 1. Я ввожу букву 
+// 2. Проверяю все страны у которых в названии есть эта буква (через фильтр)
+// 3. Обновляю таблицу согласно названию стран   
+console.log(e.target.value); 
+})
 
 };
 
@@ -17,37 +40,39 @@ $.ajax('https://restcountries.eu/rest/v2/all')
 console.log(data); // Просто выводим в консоль объекты с результатами
 addToDom(data);
 filterRegions(data);
+setListeners(data);
 })
 .catch(err => console.log(err)); // Если GET запрос не удался - выводим ошибку
 }
+
 
 // Фильтрация регионов через select
 function filterRegions(object) {
 
 // Создаем новый массив с регионами
-const regions = object.map(el => el.region);
+const regions = object.map(el => {
+// Если у страны нет региона, то вместо пробела оставляем "No Region"    s
+el.region ? el.region === "" : el.region = "No Region";    
+return el.region;  
+});
 
+console.log(removeDuplicates(regions));
 
-
-
-console.log(regions);
-
-regions.forEach((elem, index) => {
+removeDuplicates(regions).forEach((elem, index) => {
 // Наполняем селект регионами    
-$('#select').append(`<option value="${index}">${elem.region}</option>`);
+$('#select').append(`<option value="${elem}">${elem}</option>`);
 })
-
-
-// $('#select').change(() => {
-// const filteredItems = items.filter(item => item.price < 100);
-// })    
+ 
 }
+
+// Убираем повторы из массива
+const removeDuplicates = arr => [...new Set(arr)];
 
 
 // Добавляем свойства объекта в DOM
 const addToDom = object => {
 object.forEach((elem, index) => {
-$('#table').append(`<tr>
+$('#table tbody').append(`<tr>
 <td>${index+1}</td>
 <td>${elem.name}</td>
 <td>${elem.capital}</td>
